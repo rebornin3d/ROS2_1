b27ddd06d0f0e29f58298227d1c1ad9bebe9debe
reborn <rebornin3d@gmail.com>
2023-12-18 19:28:18 +0100

    urdf file added and package.xml edited

---
+++ build/pkg_1/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/florin/ros2_ws2/install/pkg_1'
---
+++ build/pkg_1/share/pkg_1/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/florin/ros2_ws2/build/pkg_1"
